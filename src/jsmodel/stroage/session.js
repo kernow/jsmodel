b6df919e67b3c483a71c5913c24c1c70566676be
Jamie Dyer <jamie@kernowsoul.com>
2011-07-06 15:57:50 +0100

    removed unneeded window. from sessionStorage

--- src/jsmodel/stroage/session.js
+++ src/jsmodel/stroage/session.js
@@ -1,4 +1,4 @@
-/*global Model: false */
+/*global Model: false, sessionStorage: false */
 
 /**
  * sessionStorage should work for Safari 4, Chrome 4, Firefox 3.5, IE 8 and Opera 10.5
@@ -27,18 +27,18 @@ Model.Storage.Session = {
   },
   
   setItem: function (key, value) {
-    window.sessionStorage.removeItem(key); // Added in to try and fix support on the iPad
-    window.sessionStorage.setItem(key, JSON.stringify(value));
+    sessionStorage.removeItem(key); // Added in to try and fix support on the iPad
+    sessionStorage.setItem(key, JSON.stringify(value));
   },
   
   removeItem: function (key) {
-    window.sessionStorage.removeItem(key);
+    sessionStorage.removeItem(key);
   },
   
   contains: function (key) {
     var contains_key, k;
     contains_key = false;
-    for (k = 0; k < window.sessionStorage.length; k++) {
+    for (k = 0; k < sessionStorage.length; k++) {
       if (this._key(k) == key) {
         contains_key = true;
         break;
@@ -48,21 +48,21 @@ Model.Storage.Session = {
   },
   
   count: function() {
-    return window.sessionStorage.length;
+    return sessionStorage.length;
   },
   
   clear: function () {
     var len, keys, i, k, self;
-    if(typeof window.sessionStorage.clear != "undefined"){
-      window.sessionStorage.clear();
+    if(typeof sessionStorage.clear != "undefined"){
+      sessionStorage.clear();
     }else{ // FF 3.0 doesn't support the clear method so we need to impliment it
-      len = window.sessionStorage.length;
+      len = sessionStorage.length;
       keys = [];
       for(i=0;i<len;i++){
-        keys.push(window.sessionStorage.key(i));
+        keys.push(sessionStorage.key(i));
       }
       $.each(keys, function(i, key){
-        window.sessionStorage.removeItem(key);
+        sessionStorage.removeItem(key);
       });
     }
   },
@@ -70,16 +70,16 @@ Model.Storage.Session = {
   // PRIVATE
 
   _key: function (index) {
-    if (index >= window.sessionStorage.length) {
+    if (index >= sessionStorage.length) {
       return null;
     } else {
-      return window.sessionStorage.key(index);
+      return sessionStorage.key(index);
     }
   },
 
   _get: function (key) {
     var item;
-    item = window.sessionStorage.getItem(key);
+    item = sessionStorage.getItem(key);
     if(item === null){
       return item;
     }else if(typeof item.value == 'undefined'){ // FF 3.0 impliments .value
