48fbb463af9d11fc3ba9076d5c8fd0dd067161d3
Jamie Dyer <jamie@kernowsoul.com>
2011-07-07 10:49:21 +0100

    fix: build task was not including the storage backends

--- build/jsmodel.js
+++ build/jsmodel.js
@@ -851,7 +851,7 @@ var Model = function(name, options) {
 
   model.add_reflections_for_self();
 
-  model.Storage.initialize(options.storage || Model.Storage.Memory);
+  model.Storage.initialize(options.storage || Model.Storage.Default);
 
   Model._add(model, name);
 
@@ -1486,6 +1486,206 @@ Model.Storage = {
 
   clear: function() {
     return this.engine.clear();
+  }
+
+};
+/*global Model: false */
+
+Model.Storage.Cookie = {
+
+  description:  'cookie storage',
+  keys:         [],
+
+
+  supported: function() {
+    var supported;
+    $.cookie('test_support', 'supported');
+    supported = $.cookie('test_support') == 'supported';
+    $.cookie('test_support', null);
+    return supported;
+  },
+
+  getItem: function (key) {
+    if (this.contains(key)) {
+      return JSON.parse($.cookie(key), this._reviver);
+    } else {
+      return null;
+    }
+  },
+
+  setItem: function (key, value) {
+    $.cookie(key, JSON.stringify(value));
+    if ($.inArray(key, this.keys) < 0) {
+      this.keys.push(key);
+    }
+  },
+
+  removeItem: function (key) {
+    $.cookie(key, null);
+    this.keys = $.map(this.keys, function(k, i){
+      if(key == k){
+        return null;
+      }else{
+        return k;
+      }
+    });
+  },
+
+  contains: function (key) {
+    return $.cookie(key) !== null;
+  },
+
+  count: function() {
+    return this.keys.length;
+  },
+
+  clear: function () {
+    $.each(this.keys, function(i, key){
+      $.cookie(key, null);
+    });
+    this.keys = [];
+  },
+
+
+  _reviver: function(key, value) {
+    var a;
+    if (typeof value === 'string') {
+      a = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)(Z|([+\-])(\d{2}):(\d{2}))$/.exec(value);
+      if (a) {
+        return new Date(Date.UTC( a[1], a[2] - 1, a[3], a[4], a[5], a[6]));
+      }
+    }
+    return value;
+  }
+};
+/*global Model: false */
+
+Model.Storage.Default = {
+
+  description: 'in memory storage',
+
+  supported: function() {
+    return true;
+  },
+
+  getItem: function(key) {
+    return null;
+  },
+
+  setItem: function(key, value) {
+    return true;
+  },
+
+  removeItem: function(key) {
+  },
+
+  contains: function(key) {
+    return false;
+  },
+
+  count: function() {
+    return 0;
+  },
+
+  clear: function() {
+  }
+
+};
+/*global Model: false, sessionStorage: false */
+
+
+Model.Storage.Session = {
+
+  description: 'session storage',
+
+  supported: function() {
+    try {
+      return !!window.sessionStorage.getItem;
+    } catch(e){
+      return false;
+    }
+  },
+
+  getItem: function (key) {
+    if (this.contains(key)) {
+      return JSON.parse(this._get(key), this._reviver);
+    } else {
+      return null;
+    }
+  },
+
+  setItem: function (key, value) {
+    sessionStorage.removeItem(key); // Added in to try and fix support on the iPad
+    sessionStorage.setItem(key, JSON.stringify(value));
+  },
+
+  removeItem: function (key) {
+    sessionStorage.removeItem(key);
+  },
+
+  contains: function (key) {
+    var contains_key, k;
+    contains_key = false;
+    for (k = 0; k < sessionStorage.length; k++) {
+      if (this._key(k) == key) {
+        contains_key = true;
+        break;
+      }
+    }
+    return contains_key;
+  },
+
+  count: function() {
+    return sessionStorage.length;
+  },
+
+  clear: function () {
+    var len, keys, i, k, self;
+    if(typeof sessionStorage.clear != "undefined"){
+      sessionStorage.clear();
+    }else{ // FF 3.0 doesn't support the clear method so we need to impliment it
+      len = sessionStorage.length;
+      keys = [];
+      for(i=0;i<len;i++){
+        keys.push(sessionStorage.key(i));
+      }
+      $.each(keys, function(i, key){
+        sessionStorage.removeItem(key);
+      });
+    }
+  },
+
+
+  _key: function (index) {
+    if (index >= sessionStorage.length) {
+      return null;
+    } else {
+      return sessionStorage.key(index);
+    }
+  },
+
+  _get: function (key) {
+    var item;
+    item = sessionStorage.getItem(key);
+    if(item === null){
+      return item;
+    }else if(typeof item.value == 'undefined'){ // FF 3.0 impliments .value
+      return item;
+    } else {
+      return item.value;
+    }
+  },
+
+
+  _reviver: function(key, value) {
+    var a;
+    if (typeof value === 'string') {
+      a = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)(Z|([+\-])(\d{2}):(\d{2}))$/.exec(value);
+      if (a) {
+        return new Date(Date.UTC( a[1], a[2] - 1, a[3], a[4], a[5], a[6]));
+      }
+    }
+    return value;
   }
 
 };
